a3780788a22d5bfe33f025a8fdbe7dcf140a52a7
Jaswanth Vardireddy <91362964+vjkr2001@users.noreply.github.com>
2023-05-26 18:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,33 @@ import feature_extraction as fe
 from bs4 import BeautifulSoup
 import requests as re
 import matplotlib.pyplot as plt
+from PIL import Image
+image = Image.open('images.jpeg')
+
+st.image(image)
 
 # col1, col2 = st.columns([1, 3])
 st.title('Final Year Project- 2019-23 Batch, Kalasalingam University')
 st.title('Implement a deep learning based system that detects whether a website is fake.')
 
+st.subheader('**Developed By:**')
+st.write('GEJJALA LAKSHMIPATHI - 9919005070')
+st.write('VARDIREDDY JASWANTH KUMAR REDDY - 9919005224')
+st.write('DUGGISETTY BALAMANIKANTA ESWAR - 9919005278')
+
 with st.expander("PROJECT DETAILS"):
-    st.subheader('Approach')
-    st.write('We have used _supervised learning_ to classify phishing and legitimate websites. '
-             'It benefit from content-based approach and we firstly focus on html of the websites. '
-             'And also, we used scikit-learn for the ML models.'
-             )
-    st.write('For this project, '
-             'We created our own data set and defined features, some from the literature and some based on manual analysis. '
-             'we used requests library to collect data, BeautifulSoup module to parse and extract features. ')
+    st.subheader('**Approach**')
+    st.write('* We have used _supervised learning_ to classify phishing and legitimate websites.')
+    st.markdown('* It benefit from content-based approach and we firstly focus on html of the websites.')
+    st.markdown('* And also, we used scikit-learn for the ML models.')
+    
+    st.markdown('* For this project,We created our own data set and defined features, some from the literature and some based on manual analysis.')
+    st.markdown('* we used requests library to collect data, BeautifulSoup module to parse and extract features.')
     st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/vjkr2001/Detection_of_Phishing_Websites_')
+    st.write('**_https://github.com/vjkr2001/Detection_of_Phishing_Websites_**')
 
     st.subheader('Data set')
-    st.write('We have used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
+    st.markdown('* We have used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
     st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
     st.write('Data set was created in November 2022.')
 
@@ -59,13 +67,13 @@ with st.expander("PROJECT DETAILS"):
     )
 
     st.subheader('Features')
-    st.write('Firstly, we have used only content-based features. Still we didn\'t use url-based faetures like length of url etc.'
-             'Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
+    st.markdown('* Firstly, we have used only content-based features. Still we didn\'t use url-based faetures like length of url etc.')
+    st.markdown('* Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
 
     st.subheader('Results')
-    st.write('We have used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
-             'Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.'
-             'Comparison table is below:')
+    st.markdown('* We have used 7 different ML classifiers of **scikit-learn** and tested them implementing **k-fold cross validation**.')
+    st.markdown('* Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.')
+    st.write('**Comparison table is below:**')
     st.table(ml.df_results)
     st.write('NB --> Gaussian Naive Bayes')
     st.write('SVM --> Support Vector Machine')
@@ -76,9 +84,9 @@ with st.expander("PROJECT DETAILS"):
     st.write('KN --> K-Neighbours')
 
 with st.expander('LETS CHECK WITH SOME OF THE PHISHING URLs:'):
-    st.write('_https://rtyu38.godaddysites.com/_')
-    st.write('_https://karafuru.invite-mint.com/_')
-    st.write('_https://defi-ned.top/h5/#/_')
+    st.write('_https://ww3.4movierulz.to/_')
+    st.write('_https://form.jotform.com/230093869703865_')
+    st.write('_https://www.cryptocurrencystate.net/_')
     st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES HERE MENTIONED ARE GIVEN IN DATASET AND EXAMPLES SHOULD BE UPDATED AS WE UPDATE DATASET!')
 
 choice = st.selectbox("Please select your machine learning model",
@@ -133,7 +141,3 @@ if st.button('Check!'):
 
     except re.exceptions.RequestException as e:
         print("--> ", e)
-
-
-
-
